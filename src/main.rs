7ca4a4e5cf4fbec9ddcfc53214906b43dc14e932
Jens van Hellemondt <jens@invictus.codes>
2024-09-19 20:38:13 +0200

    test: test_three_should_be_fizz

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_three_should_be_fizz() {
+        assert_eq!(FizzBuzzChecker::is_a_fizzbuzz(3), "Fizz");
+    }
+}
